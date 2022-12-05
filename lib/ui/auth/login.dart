d80e959e8e6f9a6cae73ca7e2dc685109ee33b19
Sahim98 <sahimsalem321@gmail.com>
2022-12-05 21:07:08 +0600

    login

--- lib/ui/auth/login.dart
+++ lib/ui/auth/login.dart
@@ -37,6 +37,7 @@ class _LoginState extends State<Login> {
           return true;
         },
         child: Scaffold(
+          resizeToAvoidBottomInset: false,
           appBar: AppBar(
             automaticallyImplyLeading: false,
             backgroundColor: Colors.amber,
@@ -50,101 +51,103 @@ class _LoginState extends State<Login> {
               ),
             ),
           ),
-          body: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Form(
-                    key: _formfield,
-                    child: Column(
+          body: SingleChildScrollView(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Form(
+                      key: _formfield,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            validator: (value) {
+                              if (value!.isEmpty)
+                                return 'E-mail is required';
+                              else
+                                return null;
+                            },
+                            controller: emailController,
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.email),
+                              labelText: 'E-mail',
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15)),
+                              ),
+                              filled: true,
+                              fillColor: Color.fromARGB(255, 211, 208, 208),
+                            ),
+                            keyboardType: TextInputType.text,
+                            maxLength: 30,
+                            autocorrect: true,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            validator: (value) {
+                              if (value!.isEmpty)
+                                return 'Password is required';
+                              else
+                                return null;
+                            },
+                            obscureText: true,
+                            obscuringCharacter: '*',
+                            controller: passController,
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.security),
+                              labelText: 'Password',
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                              ),
+                              filled: true,
+                              fillColor: Color.fromARGB(255, 211, 208, 208),
+                            ),
+                            keyboardType: TextInputType.text,
+                            maxLength: 10,
+                            autocorrect: true,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.amber, // background
+                        onPrimary: Colors.white, // foreground
+                      ),
+                      onPressed: () {
+                        if (_formfield.currentState!.validate()) {}
+                      },
+                      child: Text('Login'),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        TextFormField(
-                          validator: (value) {
-                            if (value!.isEmpty)
-                              return 'E-mail is required';
-                            else
-                              return null;
-                          },
-                          controller: emailController,
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.email),
-                            labelText: 'E-mail',
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15)),
-                            ),
-                            filled: true,
-                            fillColor: Color.fromARGB(255, 211, 208, 208),
-                          ),
-                          keyboardType: TextInputType.text,
-                          maxLength: 30,
-                          autocorrect: true,
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        TextFormField(
-                          validator: (value) {
-                            if (value!.isEmpty)
-                              return 'Password is required';
-                            else
-                              return null;
-                          },
-                          obscureText: true,
-                          obscuringCharacter: '*',
-                          controller: passController,
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.security),
-                            labelText: 'Password',
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10)),
-                            ),
-                            filled: true,
-                            fillColor: Color.fromARGB(255, 211, 208, 208),
-                          ),
-                          keyboardType: TextInputType.text,
-                          maxLength: 10,
-                          autocorrect: true,
-                        ),
+                        Text("Don't have an account?"),
+                        TextButton(
+                            onPressed: () {
+                              Navigator.push(context, MaterialPageRoute(
+                                builder: (context) {
+                                  return SignUp();
+                                },
+                              ));
+                            },
+                            child: Text('Sign up'))
                       ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.amber, // background
-                      onPrimary: Colors.white, // foreground
-                    ),
-                    onPressed: () {
-                      if (_formfield.currentState!.validate()) {}
-                    },
-                    child: Text('Login'),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Don't have an account?"),
-                      TextButton(
-                          onPressed: () {
-                            Navigator.push(context, MaterialPageRoute(
-                              builder: (context) {
-                                return SignUp();
-                              },
-                            ));
-                          },
-                          child: Text('Sign up'))
-                    ],
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),

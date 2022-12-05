d982919725b53d2c51f4a9c16af6bdc53aae146c
Sahim98 <sahimsalem321@gmail.com>
2022-12-05 21:42:23 +0600

    logged in

--- lib/firebase services/splash services.dart
+++ lib/firebase services/splash services.dart
@@ -1,13 +1,35 @@
 import 'dart:async';
-import 'package:facecam/ui/launch.dart';
+import 'package:facecam/ui/auth/tensorflow.dart';
+import 'package:facecam/launch.dart';
 import 'package:facecam/ui/auth/home.dart';
 import 'package:facecam/ui/auth/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class splashServices {
   void isLogin(BuildContext context) {
-    Timer(
+    final _auth = FirebaseAuth.instance;
+    final user = _auth.currentUser;
+
+    if(user != null)
+    {
+       Timer(
+      const Duration(seconds: 4),
+      () {
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) {
+            return Tensorflow();
+          },
+        ));
+      },
+    );
+      
+
+    }
+
+    else{
+       Timer(
       const Duration(seconds: 4),
       () {
         Navigator.push(context, MaterialPageRoute(
@@ -17,5 +39,7 @@ class splashServices {
         ));
       },
     );
+    }
+   
   }
 }
